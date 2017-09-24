13b375ba8535499b059e9f4787c484e7acfd3ad3
Monte Johnston <johnston.monte@gmail.com>
2017-09-24 19:19:48 -0400

    Update gitignore

--- target/debug/sm2-plus.d
+++
@@ -1,1 +0,0 @@
-/Users/monte/Developer/rust/sm2-plus/target/debug/sm2-plus: /Users/monte/Developer/rust/sm2-plus/src/main.rs
--- target/rls/debug/deps/sm2_plus-f11b4dddebaacf0c.d
+++
@@ -1,5 +0,0 @@
-/Users/monte/Developer/rust/sm2-plus/target/rls/debug/deps/sm2_plus-f11b4dddebaacf0c.rmeta: src/main.rs
-
-/Users/monte/Developer/rust/sm2-plus/target/rls/debug/deps/sm2_plus-f11b4dddebaacf0c.d: src/main.rs
-
-src/main.rs:
